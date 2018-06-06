2101ba0be46ec63ccb1247bd003e6110c43cceeb
peter <peter@soez.io>
2018-06-06 17:35:34 +0800

    主页添加sample-ckeditor组件

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { FormsModule } from '@angular/forms';
+import { CKEditorModule, CKEditorComponent } from 'ng2-ckeditor';
+import { SampleCkeditor } from './ckeditor.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleCkeditor
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CKEditorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
